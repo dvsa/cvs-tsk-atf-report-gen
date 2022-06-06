78dbe33ed5e218cce4658aae2e615a5342bef5aa
Melanie Coslett <melanie.coslett@dvsa.gov.uk>
2022-06-06 10:36:42 +0100

    feat(VTA-610): unit tests

--- tests/unit/ActivitiesService.unitTest.ts
+++ tests/unit/ActivitiesService.unitTest.ts
@@ -1,14 +1,15 @@
 import { ActivitiesService } from "../../src/services/ActivitiesService";
 import { wrapLambdaResponse } from "../util/responseUtils";
 import waitResponse from "../resources/wait-time-response.json";
+import visitResponse from "../resources/visit-response.json";
 import { LambdaService } from "../../src/services/LambdaService";
 import mockConfig from "../util/mockConfig";
 
 describe("Activities Service", () => {
   mockConfig();
   describe("getActivities function", () => {
-    context("Lambda client returns a single record in expected format", () => {
-      it("returns parsed result", async () => {
+    context("Lambda client returns a single wait time record in expected format", () => {
+      it("returns parsed wait time result", async () => {
         const mockLambdaService = jest.fn().mockImplementation(() => {
           return {
             invoke: () => Promise.resolve(wrapLambdaResponse(JSON.stringify(waitResponse))),
@@ -25,6 +26,43 @@ describe("Activities Service", () => {
         //     waitReason: "Break",
         //   },
         // ]);
+      });
+    });
+
+    context("Lambda client returns a single visit record in expected format", () => {
+      it("returns parsed visit result", async () => {
+        const mockLambdaService = jest.fn().mockImplementation(() => {
+          return {
+            invoke: () => Promise.resolve(wrapLambdaResponse(JSON.stringify(waitResponse))),
+            // invoke: () => Promise.resolve(wrapLambdaResponse(JSON.stringify(visitResponse))),
+            validateInvocationResponse: LambdaService.prototype.validateInvocationResponse,
+          };
+        });
+        const activityService = new ActivitiesService(new mockLambdaService());
+        const result = await activityService.getActivities({});
+        // expect(result).toEqual([
+        //   {
+        //     testerStaffId: "132",
+        //     testerName: "Gica",
+        //     testStationName: "Rowe, Wunsch and Wisoky",
+        //     activityDay: "2018-02-13",
+        //     testStationPNumber: "87-1369561",
+        //     testStationType: "gvts",
+        //     startTime: "2018-02-13T04:00:40.561Z",
+        //     activityType: "visit",
+        //     testerEmail: "tester@dvsa.gov.uk",
+        //     endTime: null,
+        //     testStationEmail: "teststationname@dvsa.gov.uk",
+        //     id: "5e4bd304-446e-4678-8289-d34fca9256e9"
+        //   }
+        // ]);
+        expect(result).toEqual([
+          {
+            startTime: "2019-01-14T10:42:33.987Z",
+            endTime: "2019-01-14T10:48:33.987Z",
+            waitReason: "Break",
+          },
+        ]);
       });
     });
 
