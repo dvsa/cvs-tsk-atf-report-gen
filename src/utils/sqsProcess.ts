27433e44ad7c9cb3c60069821957c15c49c98830
naathanbrown <nathancjb@googlemail.com>
2024-03-28 15:48:29 +0000

    feat(cb2-10603): debug

--- src/utils/sqsProcess.ts
+++ src/utils/sqsProcess.ts
@@ -2,6 +2,18 @@ import DynamoDB from "aws-sdk/clients/dynamodb";
 
 export const processRecord = (record: any) => {
     const recordBody = JSON.parse(record.body).Message;
+    console.log("record body is, ", recordBody);
+    console.log("record eventName is, ", recordBody.eventName);
+    console.log("record dynamodb is, ", recordBody.dynamodb);
+    console.log("record new image is, ", recordBody.dynamodb.NewImage);
+    console.log(recordBody.eventName === "MODIFY");
+    console.log(!!recordBody.dynamodb);
+    console.log(!!recordBody.dynamodb.NewImage);
+    console.log(
+        recordBody.eventName === "MODIFY"
+        && recordBody.dynamodb
+        && recordBody.dynamodb.NewImage
+    );
     if (
       recordBody.eventName === "MODIFY"
       && recordBody.dynamodb
