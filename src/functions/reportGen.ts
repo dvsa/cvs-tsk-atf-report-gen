20262be5fdbc34de058abd42f840430c7c7f4702
naathanbrown <nathancjb@googlemail.com>
2024-06-12 11:17:17 +0100

    feat(cb2-12588): update to work with new sns/sqs format

--- src/functions/reportGen.ts
+++ src/functions/reportGen.ts
@@ -1,8 +1,8 @@
-import { processRecord } from "@dvsa/cvs-microservice-common/functions/sqsFilter";
-import { Callback, Context, Handler } from "aws-lambda";
-import { ServiceException } from "@smithy/smithy-client";
 import { LambdaClient } from "@aws-sdk/client-lambda";
 import { PutObjectRequest } from "@aws-sdk/client-s3";
+import { processRecord } from "@dvsa/cvs-microservice-common/functions/sqsFilter";
+import { ServiceException } from "@smithy/smithy-client";
+import { Callback, Context, Handler } from "aws-lambda";
 import { ERRORS } from "../assets/enum";
 import { ActivitiesService } from "../services/ActivitiesService";
 import { LambdaService } from "../services/LambdaService";
@@ -28,7 +28,7 @@ const reportGen: Handler = async (event: any, context?: Context, callback?: Call
   const sendATFReport: SendATFReport = new SendATFReport();
 
   event.Records.forEach((record: any) => {
-    const recordBody = JSON.parse(JSON.parse(record?.body)?.Message);
+    const recordBody = JSON.parse(record?.body)
     const visit: any = processRecord(recordBody);
 
     if (visit) {
