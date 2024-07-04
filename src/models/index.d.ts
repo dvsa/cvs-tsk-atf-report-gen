099d25c6b2d10a3dc89d1f708888bccbaed5a5b7
Daniel Searle <daniel.searle1@dvsa.gov.uk>
2024-07-04 11:45:58 +0100

    feat(CB2-12690): removed commented out code

--- src/models/index.d.ts
+++ src/models/index.d.ts
@@ -33,43 +33,6 @@ interface IMOTConfig {
   templateId: string;
 }
 
-// interface IActivity {
-//   id: string;
-//   activityType: "visit" | "wait";
-//   testStationName: string;
-//   testStationPNumber: string;
-//   testStationEmail: string;
-//   testStationType: StationType;
-//   testerName: string;
-//   testerStaffId: string;
-//   startTime: string;
-//   endTime: string;
-//   waitReason: [string];
-//   notes: string;
-// }
-
-// interface ITestType {
-//   testTypeStartTimestamp: string;
-//   testTypeName: string;
-//   testResult: string;
-//   certificateNumber: string;
-//   testExpiryDate: number;
-//   testTypeEndTimeStamp: string;
-// }
-//
-// interface ITestResults {
-//   testerStaffId: string;
-//   vrm: string;
-//   testStationPNumber: string;
-//   preparerId: string;
-//   numberOfSeats: number;
-//   testStartTimestamp: string;
-//   testEndTimestamp: string;
-//   testTypes: ITestType;
-//   vin: string;
-//   vehicleType: string;
-// }
-
 interface IInvokeConfig {
   params: { apiVersion: string; endpoint?: string };
   functions: { testResults: { name: string }; testStations: { name: string; mock: string }; getActivities: { name: string } };
